b7da8913b892728d823cd19c3c940aec47bdb742
VoxelPrismatic <voxelprismatic@pm.me>
2025-03-03 14:51:06 -0600

    begin rewrite

--- lua/rabbit/docs/nvim.lua
+++ lua/rabbit/docs/nvim.lua
@@ -1,4 +1,4 @@
----@class NvimHlKwargs
+---@class NvimHlKwargs: vim.api.keyset.highlight
 ---@field fg? string Foreground color name or "#RRGGBB"
 ---@field bg? string Background color name or "#RRGGBB"
 ---@field sp? string Space color name or "#RRGGBB"
@@ -21,14 +21,12 @@
 ---@field cterm? NvimHlKwargs cterm attr map
 ---@field force? boolean Update the highlight group if it exists
 
-
 ---@class NvimEvent
 ---@field buf integer Buffer ID
 ---@field event string Event type
 ---@field file string File name
 ---@field id integer Event ID
 ---@field match string
-
 
 ---@class NvimEvent.BufAdd: NvimEvent
 ---@field buf integer Buffer ID or newly added buffer
@@ -37,14 +35,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.BufEnter: NvimEvent
 ---@field buf integer Buffer ID of buffer being entered
 ---@field event "BufEnter" Event type
 ---@field file string File name of buffer being entered
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
 
 ---@class NvimEvent.BufDelete: NvimEvent
 ---@field buf integer Buffer ID of buffer being deleted (or hidden)
@@ -53,14 +49,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.BufLeave: NvimEvent
 ---@field buf integer Buffer ID of buffer being left
 ---@field event "BufLeave" Event type
 ---@field file string File name of buffer being left
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
 
 ---@class NvimEvent.BufNew: NvimEvent
 ---@field buf integer Buffer ID of newly made buffer
@@ -69,14 +63,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.WinEnter: NvimEvent
 ---@field buf integer Focused buffer ID
 ---@field event "WinEnter" Event type
 ---@field file string File name of focused buffer
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
 
 ---@class NvimEvent.WinLeave: NvimEvent
 ---@field buf integer Focused buffer ID
@@ -85,14 +77,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.WinNew: NvimEvent
 ---@field buf integer Focused buffer ID
 ---@field event "WinNew" Event type
 ---@field file string File name of focused buffer
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
 
 ---@class NvimEvent.WinScrolled: NvimEvent
 ---@field buf integer Buffer ID
@@ -101,14 +91,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.WinClosed: NvimEvent
 ---@field buf integer Buffer ID
 ---@field event "WinScrolled" Event type
 ---@field file string ID of the window being closed, as a string
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
 
 ---@class NvimEvent.WinResized: NvimEvent
 ---@field buf integer Buffer ID
@@ -117,14 +105,12 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.ColorScheme: NvimEvent
 ---@field buf integer Buffer ID
 ---@field event "ColorScheme" Event type
 ---@field file "" Always empty string
 ---@field id integer Event ID
 ---@field match string Name of the color scheme being applied
-
 
 ---@class NvimEvent.ColorSchemePre: NvimEvent
 ---@field buf integer Buffer ID
@@ -133,7 +119,6 @@
 ---@field id integer Event ID
 ---@field match string Name of the previous color scheme
 
-
 ---@class NvimEvent.BufFilePre: NvimEvent
 ---@field buf integer Affected Buffer ID
 ---@field event "BufFilePre" Event type
@@ -141,11 +126,9 @@
 ---@field id integer Event ID
 ---@field match string Same as `file`
 
-
 ---@class NvimEvent.BufFilePost: NvimEvent
 ---@field buf integer Affected Buffer ID
 ---@field event "BufFilePost" Event type
 ---@field file string New filename
 ---@field id integer Event ID
 ---@field match string Same as `file`
-
