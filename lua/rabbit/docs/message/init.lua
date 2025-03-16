11a04588e7f9086bf8a785d1c581a4b01d211bb7
VoxelPrismatic <voxelprismatic@pm.me>
2025-03-16 13:48:19 -0500

    begin docs

--- lua/rabbit/docs/message/init.lua
+++
@@ -1,3 +0,0 @@
----@class Rabbit.Message
----@field class "message"
----@field type string
--- lua/rabbit/term/init.lua
+++
@@ -1,7 +0,0 @@
-return {
-	CTX = require("rabbit.term.ctx"),
-	BOX = require("rabbit.term.border"),
-	HL = require("rabbit.term.highlight"),
-	RECT = require("rabbit.term.rect"),
-	UI = require("rabbit.term.listing"),
-}
