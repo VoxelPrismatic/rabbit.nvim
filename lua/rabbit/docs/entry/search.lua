78ac656cdafc9868cb1a94d27bb954a11c382dd6
VoxelPrismatic <voxelprismatic@pm.me>
2025-04-21 19:02:58 -0500

    woops

--- lua/rabbit/docs/entry/search.lua
+++ lua/rabbit/docs/entry/search.lua
@@ -3,14 +3,10 @@
 ---@field label Rabbit.Entry._.Label The entry label.
 ---@field fields Rabbit.Entry.Search.Fields[]
 ---@field actions Rabbit.Entry.Collection.Actions
+---@field open integer Which field is currently open
 
 ---@class Rabbit.Entry.Search.Fields
 ---@field name string Name of the field, eg "search" or "filter".
----@field default string Text content to pre-fill when the window is spawned.
+---@field content string Text content to pre-fill when the window is spawned.
 ---@field icon string Icon to display after the search box,
 ---@field on_switch (fun(): Rabbit.Response)? A function that is called when the user switches to this field.
----@field status (fun(): Rabbit.Entry.Search.Status)? A function that returns the status of the search box. This field is automatically provided by Rabbit for your convenience.
-
----@class Rabbit.Entry.Search.Status
----@field content string Text content of the search box
----@field open boolean Whether the search box is currently open
