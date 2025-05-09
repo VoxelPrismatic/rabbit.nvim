9852d9f3edccb27f861971771d7455e7fcf9d98a
VoxelPrismatic <voxelprismatic@pm.me>
2025-05-09 15:03:34 -0500

    pass more args

--- lua/rabbit/util/nvim.lua
+++ lua/rabbit/util/nvim.lua
@@ -26,15 +26,21 @@ NVIM.ft = setmetatable({}, {
 	end,
 })
 
--- Shorthand for function() return <cb>(...) end
+-- Shorthand for function(:::) return cb(..., :::) end
+-- `:::` is yet another vararg that will be passed after the initial.
 ---@generic T
 ---@param cb fun(...): T Callback
 ---@param ...any any Arguments to pass to the callback
----@return fun(): T
+---@return fun(...): T bound
 function NVIM.bind(cb, ...)
 	local args = { ... }
-	return function()
-		return cb(unpack(args))
+	return function(...)
+		local all_args = { unpack(args) }
+		for _, arg in ipairs({ ... }) do
+			table.insert(all_args, arg)
+		end
+
+		return cb(unpack(all_args))
 	end
 end
 
